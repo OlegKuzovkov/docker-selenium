03955c0dcd7ca717988169bcdb2556497dbe8e69
olegkuzovkov <kuzovkov.oleg@gmail.com>
2016-07-09 22:29:20 +0000

    Added GET_VIDEO command support

--- NodeFirefoxDebug/bin/EchoServer.py
+++ NodeFirefoxDebug/bin/EchoServer.py
@@ -1,6 +1,26 @@
-import socket, os, time
+import socket
+import os
+import time
+import subprocess
 from shutil import copyfile
 from datetime import datetime, timedelta
+
+
+def wait_for_process_exists(process_name, wait_time=10, exists=True):
+    start_time = datetime.now()
+    out = False
+    while True:
+        console_output = str(subprocess.check_output(["ps", "-A"]))
+        if exists:
+            if process_name in console_output:
+                out = True
+        else:
+            if process_name not in console_output:
+                out = True
+        if datetime.now() - start_time > timedelta(seconds=wait_time) or out:
+            break
+        time.sleep(0.1)
+    return out
 
 max_video_wait_time = 30
 acync_process = None
@@ -10,7 +30,7 @@ port = 11111
 default_temp_video_file_location = "opt/bin/video/temp/temp_video.mp4"
 default_video_file_location = "opt/bin/video/video.mp4"
 connection_socket.bind((host, port))
-buffer_size = 1024
+buffer_size = 2048
 connection_socket.listen(5)
 while True:
     connection, address = connection_socket.accept()
@@ -19,15 +39,21 @@ while True:
     if 'START_VIDEO' in command:
         command = command[len('START_VIDEO')+4:len(command)-2] + " %s &" % default_temp_video_file_location
         os.system(command)
+        if not wait_for_process_exists('ffmpeg'):
+            print("Unable to start video recording")
     elif 'STOP_VIDEO' in command:
         command = command[len('STOP_VIDEO')+4:len(command)-2]
         os.system(command)
+        if not wait_for_process_exists('ffmpeg', exists=False):
+            print("Unable to stop video recording")
+    elif 'GET_VIDEO':
         if os.path.isfile(default_temp_video_file_location):
             start_time = datetime.now()
             while True:
                 copyfile(default_temp_video_file_location, default_video_file_location)
                 time.sleep(1)
                 if os.path.getsize(default_video_file_location) == os.path.getsize(default_temp_video_file_location):
+                    os.remove(default_temp_video_file_location)
                     break
                 if datetime.now() - start_time > timedelta(seconds=max_video_wait_time):
                     break
@@ -37,9 +63,6 @@ while True:
                 connection.send(file_bytes)
                 file_bytes = file_obj.read(buffer_size)
             file_obj.close()
-            time.sleep(1)
-            os.remove(default_temp_video_file_location)
-            time.sleep(1)
             os.remove(default_video_file_location)
     connection.close()
 
